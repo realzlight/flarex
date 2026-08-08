261c898085fbc65d5eb110584d433fbf1b72bd9c
Your Name <your@email.com>
2026-08-08 20:26:55 +0530

    safest

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import os from "os"
 import fs from "fs"
 import { setTimeout as sleep } from "node:timers/promises";
 import gradient from "gradient-string"
+import boxen from "boxen"
+
 const program = new Command();
 
 const FLAREX_DIR = path.join(os.homedir(), "FlarexProjects");
@@ -30,7 +32,11 @@ function center(text) {
 }
 
 
-
+function msg(txt){
+console.log()
+console.log(boxen(chalk.red(txt)))
+console.log()
+}
 
 function loadConfig() {
   if (!fs.existsSync(CONFIG_PATH)) return null; 
@@ -54,7 +60,7 @@ function isInit() {
 
 
 async function banner() {
-const graySandwich = gradient(['#4a4a4a', '#ffffff', '#4a4a4a']);
+const graySandwich = gradient(['#666666', '#ffffff', '#666666']);
   const config =  loadConfig()
 const theme = config?.user?.theme || ['cyan', 'magenta']
   await clearScreen();
@@ -68,6 +74,7 @@ const theme = config?.user?.theme || ['cyan', 'magenta']
   });
 
   console.log(center(chalk.bold("Fastest Way to Scaffold and Manage Projects")));
+  console.log(center(chalk.dim("> RUN flarex tutorial")));
   console.log();
 
   if (fs.existsSync(CONFIG_PATH)) {
@@ -82,8 +89,13 @@ const theme = config?.user?.theme || ['cyan', 'magenta']
       ];
       const greeting = greetings[Math.floor(Math.random() * greetings.length)];
       console.log()
+
+
       console.log(center(chalk.bold(graySandwich(greeting))));
-    } else {
+
+
+
+  } else {
       console.log(center(chalk.gray.bold("> Run 'flare init' to Lock In!")));
     }
   } else {
@@ -191,6 +203,140 @@ await sleep(3000)
 banner()
 
 }); // PARENT CLOSING
+
+
+
+program
+  .command("create")
+  .description("CREATE AND SETUP PROJECT COMPLETELY AND STORE IN FlareProjects FOLDER")
+  .action(async () => {
+
+  if (isInit()){
+
+        console.log()
+        clack.intro("Flarex Locking in... ")
+
+
+const { projectname, template } = await clack.group({
+  projectname: () => clack.text({
+    message: "What Would You Name Your Project?",
+    placeholder: "MyApp...",
+    validate: (v) => !v && "Project Name Is Required"
+  }),
+
+  template: () => clack.select({
+    message: "What Is Your Project Type",
+    options: [
+      { value: 'MERN', label: "Mern Stack" },
+      { value: 'Express', label: "Express" },
+      { value: 'React', label: "React + Vite" }
+    ],
+  }),
+}, {
+  onCancel: () => {
+    clack.cancel("Setup cancelled");
+    process.exit(0);
+  }
+});
+        if (template === "MERN"){
+                console.log()
+                console.log(chalk.bold(`Flarex Is Starting to Set Up ${projectname}`))
+                console.log()
+                const s = clack.spinner()
+                s.start("Flarex Is Cooking...")
+                console.log()
+                const safeName = projectname.trim().replace(/\s+/g, '-').toLowerCase();
+                const FLAREX_DIR = path.join(os.homedir(), "FlarexProjects");
+                const FullPath = path.join(FLAREX_DIR,safeName)
+                fs.mkdirSync(FullPath, {recursive:true})
+
+                console.log(chalk.bold(`• ✓ Flarex Created ${safeName}`))
+
+                const serverPath = path.join(FullPath,"server")
+                fs.mkdirSync(serverPath, {recursive:true})
+                await execa("npm",["init","-y"],{stdio:"ignore",cwd:serverPath})
+
+                const packageJsonPath = path.join(serverPath,"package.json")
+                const packageJson = JSON.parse(fs.readFileSync(packageJsonPath,"utf8"))
+
+                packageJson.name = safeName;
+                packageJson.type = "module";
+                packageJson.description = "Made with Flarex";
+                packageJson.scripts = {
+                  ...packageJson.scripts,
+                          dev: `node index.js`,
+                          start: `node index.js`
+                        };
+
+                fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+                console.log(chalk.bold("• ✓ Flarex Created and Configured 'package.json'"))
+
+
+                // WRTING GITIGNORE
+                const gitignorePath = path.join(FullPath, '.gitignore');
+
+const gitignoreContent = `node_modules/
+dist/
+.env
+.DS_Store
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+`;
+
+                fs.writeFileSync(gitignorePath, gitignoreContent);
+                console.log(chalk.bold("• ✓ Flarex Configured '.gitignore'"))
+
+
+                // INDEX.JS
+fs.writeFileSync(
+path.join(serverPath, 'index.js'),
+`import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Flarex server is running 🚀' });
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(\`Server running on http://localhost:\${PORT}\`);
+});
+`
+);
+
+
+
+                s.stop("Flarex Is Done Cooking!!")
+}
+
+
+
+        console.log()
+        clack.outro(chalk.green("Flarex Set Up Everything For You.."))
+  }else{
+        msg("!Init")
+  }
+
+
+}) // Parent Closing
+
+
+
+
+
+
+
+
 // MAIN
 async function main(){
   program.parse();
